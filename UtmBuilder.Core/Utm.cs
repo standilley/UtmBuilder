d255a5bf77dcfb76f88dbfae1e3b23e12524000f
standilley <standilleyoliveira@gmail.com>
2024-03-21 22:52:38 -0300

    add Extension Methods - Implicit Operators

--- UtmBuilder.Core/Utm.cs
+++ UtmBuilder.Core/Utm.cs
@@ -24,6 +24,29 @@ namespace UtmBuilder.Core
         /// </summary>
         public Campaign Campaign { get;}
 
+        public static implicit operator string(Utm utm) => utm.ToString();
+        public static implicit operator Utm(string link)
+        {
+            if(string.IsNullOrEmpty(link))
+                throw new InvalidUrlException();
+            var url = new Url(link);
+            var segments = url.Address.Split("?"); // separar a url em duas na "?"
+            if(segments.Length == 1)
+                throw new InvalidUrlException("No segments were provided");
+            var pars = segments[1].Split("&"); // separar em cada "&"
+
+            var source = pars.Where(x => x.StartsWith("utm_source")).FirstOrDefault("").Split("=")[1];
+            var medium = pars.Where(x => x.StartsWith("utm_medium")).FirstOrDefault("").Split("=")[1];
+            var name = pars.Where(x => x.StartsWith("utm_campaign")).FirstOrDefault("").Split("=")[1];
+            var id = pars.Where(x => x.StartsWith("utm_id")).FirstOrDefault("").Split("=")[1];
+            var term = pars.Where(x => x.StartsWith("utm_term")).FirstOrDefault("").Split("=")[1];
+            var content = pars.Where(x => x.StartsWith("utm_content")).FirstOrDefault("").Split("=")[1];
+
+            var utm = new Utm(
+                new Url(segments[0]),
+                new Campaign(source, medium, name, id, term, content));
+            return utm;
+        }
         public override string ToString()
         {
             var segments = new List<string>();
@@ -35,6 +58,14 @@ namespace UtmBuilder.Core
             segments.AddIfNotNull("utm_content", Campaign.Content);
 
             return $"{Url.Address}?{string.Join("&", segments)}";
+        }
+        public class Test
+        {
+            public void Test2()
+            {
+                var url = "https://balta.io/player/assistir/dfa3752e-4bd5-4101-bafb-ff3afcf39930/5e60adc7-0db2-4048-bed7-db6f0548f6c5";
+               string utm = (Utm)url;
+            }
         }
     }
 }
